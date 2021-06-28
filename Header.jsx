10add07349ec57758a310d07bed5f1e9847dc204
pranjwalgupta <72875884+pranjwalgupta@users.noreply.github.com>
2021-06-28 15:17:49 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Header() {
+  return (
+    <div>
+    <header>
+        <h1>ShapeAI Bootcamps</h1>
+        </header>
+    </div>
+  );
+}
+export default Header;
---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing bootcamp taken by Shaurya Sinha Sir We covered
+        everything from Scratch including Javascript,React.js,HTML.
+      </p>
+    </div>
+  );
+}
+export default Info;
